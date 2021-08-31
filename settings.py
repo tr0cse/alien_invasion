c4e1306c8ef32ab60c86068197665c9724552e90
tr0cse <gurkanakyuz75@gmail.com>
2021-08-31 22:08:08 +0300

    fullscreen

--- settings.py
+++ settings.py
@@ -7,6 +7,6 @@ class Settings:
         self.screen_width = 900
         self.screen_height = 600
         self.bg_color = (230, 230, 230)
-        self.ship_speed = 1
+        self.ship_speed = 1.5
 
         
