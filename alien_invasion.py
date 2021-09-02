65f97c9feb918100b0d0d85ffdaac4ad9d404ad0
tr0cse <gurkanakyuz75@gmail.com>
2021-09-02 15:11:07 +0300

    making ship shooting and fixing the error in bullet.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
@@ -17,6 +18,7 @@ class AlienInvasion:
         pygame.display.set_caption("Alien Invasion")
         
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         
 
     def run_game(self):
@@ -25,6 +27,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_keydown_events(self, event):        
@@ -36,6 +39,9 @@ class AlienInvasion:
         
         elif event.key == pygame.K_q:
             sys.exit()
+            
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
             
     def _check_keyup_events(self, event):        
         if event.key == pygame.K_RIGHT:
@@ -55,10 +61,17 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
                     
-
+    def _fire_bullet(self):
+        """Create a new bullet and add it to the bullets group."""
+        new_bullet = Bullet(self)
+        self.bullets.add(new_bullet)
+        
+        
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         # Make the most recently drawn screen visible.  
         pygame.display.flip()
 
