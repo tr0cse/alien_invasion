8a17a26b3f49652f36dd6d0a3f3d9a247c4bde8b
tr0cse <gurkanakyuz75@gmail.com>
2021-09-02 15:23:28 +0300

    removing bullets if it goes off the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -28,6 +28,10 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self.bullets.update()
+            
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
             self._update_screen()
 
     def _check_keydown_events(self, event):        
