2609cd97eebfefc62e28bca816ede62feed13bdf
tr0cse <gurkanakyuz75@gmail.com>
2021-09-07 23:57:16 +0300

    writing levels on the screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -98,6 +98,7 @@ class AlienInvasion:
             # Reset the game statistics.
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
             
             # Get rid of any remaining aliens and bullets.
             self.aliens.empty()
@@ -219,12 +220,18 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
+            
         
         if not self.aliens:
             # Destroy existing bullets and create new fleet.
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+            
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
     
 
     def _update_screen(self):
